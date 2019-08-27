23766fd0e9326949ded36a0b781398f543a4dc62
Pikkanet Chokwattanapornchai <pikkanet406@gmail.com>
2019-08-27 15:01:51 +0700

    new project with tap bar

---
+++ Mobile Phone Buyer Guide/Mobile Phone Buyer Guide/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Mobile Phone Buyer Guide
+//
+//  Created by Pikkanet Chokwattanapornchai on 27/8/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mobile Phone Buyer Guide/Mobile Phone Buyer Guide/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Mobile Phone Buyer Guide
+//
+//  Created by Pikkanet Chokwattanapornchai on 27/8/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
