fa1833deb2f0b89f56ddb0e7f3547a636cd0d629
Pikkanet Chokwattanapornchai <pikkanet406@gmail.com>
2019-08-27 15:14:32 +0700

    add custom tab bar

---
+++ Mobile Phone Buyer Guide/Mobile Phone Buyer Guide/CustomTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  CustomTabBarController.swift
+//  Mobile Phone Buyer Guide
+//
+//  Created by Pikkanet Chokwattanapornchai on 27/8/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews()
+    {
+        
+//        tabBar.frame = CGRectMake(0,0,UIScreen.mainScreen().bounds.width,50)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
