0ae22876f5811fbe977e818c7d2f79c7d878997b
Pikkanet Chokwattanapornchai <pikkanet406@gmail.com>
2019-08-28 11:46:19 +0700

    add collectionview on detail page

---
+++ Mobile Phone Buyer Guide/Mobile Phone Buyer Guide/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  Mobile Phone Buyer Guide
+//
+//  Created by Pikkanet Chokwattanapornchai on 28/8/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
